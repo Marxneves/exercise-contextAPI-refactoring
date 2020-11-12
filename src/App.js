580bf52e8436dd4917a7bd8c365efe39984ef0c5
Marxneves <marxkhristie@gmail.com>
2020-11-12 18:22:05 -0300

    Resolve exercise using COntext API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cars: {
+        redCar: false,
+        blueCar: false,
+        yellowCar: false,
+      },
+    };
+
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(color, change) {
+    this.setState({
+      cars: {
+         ...this.state.cars,
+        [color]: change
+    }})
+  }
+
+  render() {
+    return (
+      <MyContext.Provider value={{...this.state, moveCar: this.moveCar,}} >
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
